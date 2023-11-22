707a942b87061687957ad224af430dd48394361c
Eduardo Teixeira <eduardo.apteixeira00@gmail.com>
2023-11-22 16:28:38 -0300

    feat: swagger and docker implementation

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:latest
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+COPY tsconfig.json ./
+COPY prisma ./prisma
+COPY src ./src
+
+RUN npm install
+
+EXPOSE 3000
+
+CMD ["npm", "start"]
---
+++ swaggerConfig.ts
@@ -0,0 +1,36 @@
+import swaggerJsdoc from "swagger-jsdoc"
+
+const options: swaggerJsdoc.Options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "REST API TodoList",
+      version: "1.0.0",
+      description: "Documentação da API TodoList",
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./src/routes/*.ts"],
+}
+
+const swaggerSpec = swaggerJsdoc(options)
+
+module.exports = swaggerSpec
